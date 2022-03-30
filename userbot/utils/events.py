dc0f000892d731727b5805cbd2b6ecdc3f9ab6ee
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-30 11:01:26 +0700

    Update events.py

--- userbot/utils/events.py
+++ userbot/utils/events.py
@@ -1,7 +1,7 @@
 import pybase64
 from telethon.tl.functions.channels import JoinChannelRequest as Get
 from telethon.tl.types import MessageEntityMentionName
-
+from userbot import RAM2, RAM3, RAM4, RAM5, bot
 from .logger import logging
 from .tools import edit_delete
 
