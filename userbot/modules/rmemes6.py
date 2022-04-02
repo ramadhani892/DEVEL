9d403ebb40712b848a9a653027ec1e013fc8440c
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-03 05:57:42 +0700

    Create rmemes6.py

---
+++ userbot/modules/rmemes6.py
@@ -0,0 +1,107 @@
+from time import sleep
+
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import ram_cmd as tod
+
+
+@tod(pattern=r"heu(?: |$)(.*)")
+async def _(typew):
+    await typew.client.send_message(
+        typew.chat_id, "▒▒▒▒▒▄██████████▄▒▒▒▒▒\n"
+                     "▒▒▒▄██████████████▄▒▒▒\n"
+                     "▒▒██████████████████▒▒\n"
+                     "▒▐███▀▀▀▀▀██▀▀▀▀▀███▌▒\n"
+                     "▒███▒▒▌■▐▒▒▒▒▌■▐▒▒███▒\n"
+                     "▒▐██▄▒▀▀▀▒▒▒▒▀▀▀▒▄██▌▒\n"
+                     "▒▒▀████▒▄▄▒▒▄▄▒████▀▒▒\n"
+                     "▒▒▐███▒▒▒▀▒▒▀▒▒▒███▌▒▒\n"
+                     "▒▒███▒▒▒▒▒▒▒▒▒▒▒▒███▒▒\n"
+                     "▒▒▒██▒▒▀▀▀▀▀▀▀▀▒▒██▒▒▒\n"
+                     "▒▒▒▐██▄▒▒▒▒▒▒▒▒▄██▌▒▒▒\n"
+                     "▒▒▒▒▀████████████▀▒▒▒▒\n", reply_to=typew.reply_to_msg_id)
+    await typew.delete()
+
+@tod(pattern=r"hem(?: |$)(.*)")
+async def _(typew):
+    await typew.client.send_message(
+        typew.chat_id, " ╭━┳━╭━╭━╮╮\n"
+                     " ┃┈┈┈┣▅╋▅┫┃\n"
+                     " ┃┈┃┈╰━╰━━━━━━╮\n"
+                     " ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣\n"
+                     "   ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉\n"
+                     "   ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤\n"
+                     "   ╲┃┈┈┈┈╭━┳━━━━╯\n"
+                     "   ╲┣━━━━━━┫\n"
+                     "      **MENCURIGAKAN**\n", reply_to=typew.reply_to_msg_id)
+
+
+@tod(pattern=r"wle(?: |$)(.*)")
+async def _(typew):
+    await typew.client.send_message(
+        typew.chat_id, "┈╭━━━━━━━━━━━╮┈\n"
+                     "╭╯┈╭━━╮┈╭━━╮┈╰╮\n"
+                     "┃┈┃┃╭╮┃┈┃╭╮┃┃┈┃\n"
+                     "┃┈┃┻┻┻┛┈┗┻┻┻┃┈┃\n"
+                     " ┃┈┃╭╮┈◢▇◣┈╭╮┃┈┃\n"
+                     "╰┳╯┃╰━━┳┳┳╯┃╰┳╯\n"
+                     "┈┃┈╰━━━┫┃┣━╯┈┃┈\n"
+                     "┈┃┈┈┈┈┈╰━╯┈┈┈┃┈\n", reply_to=typew.reply_to_msg_id)
+    await typew.delete()
+
+@tod(pattern=r"peler(?: |$)(.*)")
+async def _(typew):
+    await typew.client.send_message(
+        typew.chat_id, "░░░░█─────────────█──▀──\n"
+                     "░░░░▓█───────▄▄▀▀█──────\n"
+                     "░░░░▒░█────▄█▒░░▄░█─────\n"
+                     "░░░░░░░▀▄─▄▀▒▀▀▀▄▄▀──SIJONI─\n"
+                     "░░░░░░░░░█▒░░░░▄▀────PANJANG\n"
+                     "▒▒▒░░░░▄▀▒░░░░▄▀───DAN─\n"
+                     "▓▓▓▓▒░█▒░░░░░█▄───PEMBERANI─\n"
+                     "█████▀▒░░░░░█░▀▄───CROTT──\n"
+                     "█████▒▒░░░▒█░░░▀▄─AHHH──\n"
+                     "███▓▓▒▒▒▀▀▀█▄░░░░█──────\n"
+                     "▓██▓▒▒▒▒▒▒▒▒▒█░░░░█─────\n"
+                     "▓▓█▓▒▒▒▒▒▒▓▒▒█░░░░░█────\n"
+                     "░▒▒▀▀▄▄▄▄█▄▄▀░░░░░░░█─\n", reply_to=typew.reply_to_msg_id)
+    await typew.delete()
+
+@tod(pattern=r"ahh(?: |$)(.*)")
+async def _(typew):
+    await typew.client.send_mesagge(
+        typew.chat_id, "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡠⠢⠏⣉⣉⠣⢦⡀⠄⠄⠄⠄\n"
+                     "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣐⢪⠅⣱⣟⢛⢧⠄⣷⠄⠄⠄⠄\n"
+                     "⠄⠄⠄⠄⠄⠄⠄⠠⣬⣀⣢⣕⡽⢯⣩⣳⣷⠧⡆⠄⠄⠄\n"
+                     "⠄⠄⠄⠄⠄⠄⠄⢠⡭⡾⣆⢿⡙⢟⢻⣿⡿⡹⣶⠅⠄⠄⠄ \n"
+                     "⠄⠄⠄⠄⠄⠄⠄⣔⠄⡗⢫⠘⠷⠾⠿⠟⠙⣡⢢⢖⠄⠄⠄\n"
+                     "⠄⠄⠄⠄⠄⠄⠄⢫⠺⠁⡹⢸⣿⣿⣿⡟⠲⣦⣌⢄⡇⠄⠄\n"
+                     "⠄⠄⠄⠄⠄⠄⢀⡈⢛⠨⠥⡄⣿⣿⣿⣧⢯⡻⡇⠨⣁⠄⠄\n"
+                     "⠄⠄⠄⠄⠄⢠⡤⢠⣶⣿⣿⣿⣿⣿⣿⣿⣷⢗⠵⡐⡣⡄⠄\n"
+                     "⠄⠄⠄⠄⠄⠸⢡⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣷⡄⢶⡇⡇⠄\n"
+                     "⠄⠄⠄⠄⠄⣠⣿⣿⣿⣿⣿⠟⢸⣿⣿⣿⡟⡻⣿⠘⠧⡇⠄\n"
+                     "⠄⠄⠄⠄⠄⢳⣿⣿⣿⡿⣫⣾⣜⢿⣿⣿⣷⣶⡿⢀⠗⡇⠄\n"
+                     "⠄⠄⠄⠄⠄⣦⡝⣭⣵⣾⣿⣿⣿⣦⣍⣛⣛⣫⠡⣼⢾⣿⠄\n"
+                     "⠄⠄⠄⠄⢰⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠨⣿⡟⠄\n"
+                     "⠄⠄⠄⢀⣿⡿⣿⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢆⣶⣿⠇⠄\n"
+                     "⠄⠄⠄⢸⣿⣷⠎⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⣿⣷⣻⡆⠄\n"
+                     "⠄⠄⢀⣿⣿⠏⢸⡟⢿⣿⣿⣸⣿⣿⣿⣿⢏⣾⣿⣿⣏⣷⠄\n"
+                     "⠄⠄⣸⣿⠏⢀⣼⣿⣎⡻⢿⣿⣿⣿⠿⣣⣾⣿⣿⣿⣿⣽⠄\n"
+                     "⠄⠄⣿⡿⣰⣿⣿⣿⣿⣿ AHHH ⣰⣿⣿⣿⣿⣿⣿⣿\n", reply_to=typew.reply_to_msg_id)
+    await typew.delete()
+
+CMD_HELP.update(
+    {
+        "animasi9": f"**Plugin : **`animasi8`\
+        \n\n  •  **Syntax :** `{cmd}heu`\
+        \n  •  **Function : **Mengirim Gambar Monyet.\
+        \n\n  •  **Syntax :** `{cmd}hem`\
+        \n  •  **Function : **Mengirim Gambar Anjing Mencurigakan.\
+        \n\n  •  **Syntax :** `{cmd}wle`\
+        \n  •  **Function : **Mengirim Gambar Anjing Melet.\
+        \n\n  •  **Syntax :** `{cmd}peler`\
+        \n  •  **Function : **Mengirim Gambar Peler.\
+        \n\n  •  **Syntax :** `{cmd}ahh`\
+        \n  •  **Function : **Mengirim Gambar Uhh Ahh Uhh Ahh.\
+    "
+    }
+)
