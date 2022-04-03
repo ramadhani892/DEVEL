4380882da2b50d79e87ec020ae24d7e508130c99
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-04 00:24:10 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -1,7 +1,4 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-# RAM-UBOT MINTA
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License
+# Ping From <\ram-ubot/>
 """ Userbot module containing commands related to the \
     Information Superhighway (yes, Internet). """
 
