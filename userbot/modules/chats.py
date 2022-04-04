948addb1f5453a0c365384f876f22edf9004f9b7
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-04 15:52:17 +0700

    Added Inviteall

--- userbot/modules/chats.py
+++ userbot/modules/chats.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
-#
+# from <ram-ubot/>
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 """ Userbot module containing userid, chatid and log commands"""
